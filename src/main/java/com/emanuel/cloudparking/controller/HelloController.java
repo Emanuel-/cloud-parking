cb06a584ceb84ec30d6bec9da104b0ca548843b4
Emanuel- <emanuel.albuquerque.1995@gmail.com>
2022-09-28 22:33:49 -0300

    Carga inicial do projeto

---
+++ src/main/java/com/emanuel/cloudparking/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.emanuel.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		return "Hello world 22scdasd2";
+	}
+
+}
